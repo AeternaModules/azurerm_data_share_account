0f166faed3be6984ce029e5d0589bf1eb4b7c851
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "data_share_accounts" {
-  description = "All data_share_account resources"
-  value       = azurerm_data_share_account.data_share_accounts
-}
 output "data_share_accounts_identity" {
-  description = "List of identity values across all data_share_accounts"
-  value       = [for k, v in azurerm_data_share_account.data_share_accounts : v.identity]
+  description = "Map of identity values across all data_share_accounts, keyed the same as var.data_share_accounts"
+  value       = { for k, v in azurerm_data_share_account.data_share_accounts : k => v.identity }
 }
 output "data_share_accounts_location" {
-  description = "List of location values across all data_share_accounts"
-  value       = [for k, v in azurerm_data_share_account.data_share_accounts : v.location]
+  description = "Map of location values across all data_share_accounts, keyed the same as var.data_share_accounts"
+  value       = { for k, v in azurerm_data_share_account.data_share_accounts : k => v.location }
 }
 output "data_share_accounts_name" {
-  description = "List of name values across all data_share_accounts"
-  value       = [for k, v in azurerm_data_share_account.data_share_accounts : v.name]
+  description = "Map of name values across all data_share_accounts, keyed the same as var.data_share_accounts"
+  value       = { for k, v in azurerm_data_share_account.data_share_accounts : k => v.name }
 }
 output "data_share_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all data_share_accounts"
-  value       = [for k, v in azurerm_data_share_account.data_share_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all data_share_accounts, keyed the same as var.data_share_accounts"
+  value       = { for k, v in azurerm_data_share_account.data_share_accounts : k => v.resource_group_name }
 }
 output "data_share_accounts_tags" {
-  description = "List of tags values across all data_share_accounts"
-  value       = [for k, v in azurerm_data_share_account.data_share_accounts : v.tags]
+  description = "Map of tags values across all data_share_accounts, keyed the same as var.data_share_accounts"
+  value       = { for k, v in azurerm_data_share_account.data_share_accounts : k => v.tags }
 }
 
